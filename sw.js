5794625ba609cda19ac38c9d7b81abeb42afe8bd
chuoneone <90313941+chuoneone@users.noreply.github.com>
2026-01-26 20:04:17 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+// 簡單的 Service Worker，用於滿足 PWA 安裝條件
+const CACHE_NAME = 'okinawa-trip-v1';
+const urlsToCache = [
+  './itinerary.html',
+  './manifest.json',
+  './images/japan.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
